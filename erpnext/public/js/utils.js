12cc2435b4d038e900fdf0710a5b0698591fcd75
Saif Ur Rehman <saifi0102@gmail.com>
2019-05-15 23:25:26 +0500

    fix: Remove Item Code formatter

--- erpnext/public/js/utils.js
+++ erpnext/public/js/utils.js
@@ -607,14 +607,6 @@ erpnext.utils.map_current_doc = function(opts) {
 	}
 }
 
-frappe.form.link_formatters['Item'] = function(value, doc) {
-	if(doc && doc.item_name && doc.item_name !== value) {
-		return value? value + ': ' + doc.item_name: doc.item_name;
-	} else {
-		return value;
-	}
-}
-
 frappe.form.link_formatters['Employee'] = function(value, doc) {
 	if(doc && doc.employee_name && doc.employee_name !== value) {
 		return value? value + ': ' + doc.employee_name: doc.employee_name;
