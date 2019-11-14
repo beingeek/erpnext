44d1f048fc77a02ecee8af412368e52bbf84a940
Saif Ur Rehman <saifi0102@gmail.com>
2019-11-14 17:20:41 +0500

    feat(Qty Adjust): To date calculate Short/Excess qty

--- erpnext/selling/report/qty_adjust/qty_adjust.js
+++ erpnext/selling/report/qty_adjust/qty_adjust.js
@@ -6,10 +6,15 @@ frappe.query_reports["Qty Adjust"] = {
 	"filters": [
 		{
 			fieldname: "date",
-			label: __("Date"),
+			label: __("From Date"),
 			fieldtype: "Date",
 			default: frappe.datetime.get_today(),
 			reqd: 1
+		},
+		{
+			fieldname: "selected_to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
 		},
 		{
 			fieldname: "item_code",
@@ -37,7 +42,8 @@ frappe.query_reports["Qty Adjust"] = {
 		};
 
 		if (data) {
-			if (['draft_so_qty', 'total_po_qty', 'actual_qty'].includes(column.fieldname)) {
+			if (['draft_so_qty', 'total_po_qty', 'actual_qty', 'total_selected_po_qty', 'total_selected_so_qty',
+					'total_available_qty', 'short_excess'].includes(column.fieldname)) {
 				options.css['font-weight'] = "bold";
 			}
 
@@ -50,6 +56,13 @@ frappe.query_reports["Qty Adjust"] = {
 			if (column.is_po_qty) {
 				options.link_href = encodeURI("desk#query-report/Purchase Order Items To Be Received" +
 					"?item_code=" + data.item_code + "&from_date=" + column.from_date + "&to_date=" + column.to_date);
+			}
+
+			if (column.fieldname == 'total_selected_so_qty' && flt(value) > 0) {
+				options.css['color'] = 'red';
+			}
+			if (column.fieldname == 'short_excess' && flt(value) < 0) {
+				options.css['color'] = 'red';
 			}
 		}
 
--- erpnext/selling/report/qty_adjust/qty_adjust.py
+++ erpnext/selling/report/qty_adjust/qty_adjust.py
@@ -12,8 +12,6 @@ from six import iteritems
 def execute(filters=None):
 	filters = frappe._dict(filters or {})
 	filters.date = getdate(filters.date or nowdate())
-	filters.from_date = filters.date
-	filters.to_date = frappe.utils.add_days(filters.from_date, 4)
 
 	columns = get_columns(filters)
 	data = get_data(filters)
@@ -22,90 +20,108 @@ def execute(filters=None):
 
 
 def get_data(filters):
+	template = frappe._dict({"actual_qty": 0, "total_so_qty": 0, "total_po_qty": 0, "total_selected_so_qty": 0,
+		"total_selected_po_qty": 0})
+	def add_qty_to_item_map(data, so_or_po, set_daily):
+		for d in data:
+			item_map.setdefault(d.item_code, template.copy())
+			item_map[d.item_code]['item_code'] = d.item_code
+			item_map[d.item_code]['item_name'] = d.item_name
+			item_map[d.item_code]['total_{0}_qty'.format(so_or_po)] += d.qty
+
+			if set_daily:
+				i = frappe.utils.date_diff(d.date, filters.from_date)
+				day_field = '{0}_day_{1}'.format(so_or_po, i + 1)
+				item_map[d.item_code].setdefault(day_field, 0)
+				item_map[d.item_code][day_field] += d.qty
+
 	conditions = get_item_conditions(filters)
-
-	po_data = frappe.db.sql("""
-		select
-			i.item_code, i.item_name, po.schedule_date as date,
-			sum((i.qty - ifnull(i.received_qty, 0)) * i.conversion_factor) as qty
-		from `tabPurchase Order Item` i
-		inner join `tabPurchase Order` po on po.name = i.parent
-		where po.docstatus < 2 and po.status != 'Closed' and ifnull(i.received_qty, 0) < ifnull(i.qty, 0)
-			and po.schedule_date between %(from_date)s and %(to_date)s {0}
-		group by i.item_code, po.schedule_date
-	""".format(conditions), filters, as_dict=1)
-
-	so_data = frappe.db.sql("""
-		select
-			i.item_code, i.item_name, so.delivery_date as date, so.docstatus,
-			sum((i.qty - ifnull(i.delivered_qty, 0)) * i.conversion_factor) as qty
-		from `tabSales Order Item` i
-		inner join `tabSales Order` so on so.name = i.parent
-		where so.docstatus < 2 and ifnull(i.delivered_qty, 0) < ifnull(i.qty, 0) and so.status != 'Closed'
-			and so.delivery_date between %(from_date)s and %(to_date)s {0}
-		group by i.item_code, so.delivery_date, so.docstatus
-	""".format(conditions), filters, as_dict=1)
-
-	sinv_data = frappe.db.sql("""
-		select
-			i.item_code, i.item_name, sinv.delivery_date as date,
-			sum(i.stock_qty) as qty
-		from `tabSales Invoice Item` i
-		inner join `tabSales Invoice` sinv on sinv.name = i.parent
-		where sinv.docstatus = 0 and ifnull(i.sales_order, '') = ''
-			and sinv.delivery_date between %(from_date)s and %(to_date)s {0}
-		group by i.item_code, sinv.delivery_date
-	""".format(conditions), filters, as_dict=1)
-
-	bin_data = frappe.db.sql("""
-		select bin.item_code, sum(bin.actual_qty) as actual_qty, i.item_name
-		from tabBin bin, tabItem i
-		where i.name = bin.item_code and i.is_sales_item=1 {0}
-		group by bin.item_code
-		having actual_qty != 0
-	""".format(conditions), filters, as_dict=1)
-
 	item_map = {}
-	template = frappe._dict({"actual_qty": 0, "total_so_qty": 0, "total_po_qty": 0, "draft_so_qty": 0})
 
-	for d in so_data:
-		item_map.setdefault(d.item_code, template.copy())
-		i = frappe.utils.date_diff(d.date, filters.from_date)
-		item_map[d.item_code]['item_code'] = d.item_code
-		item_map[d.item_code]['item_name'] = d.item_name
-		item_map[d.item_code]['total_so_qty'] += d.qty
-		if d.docstatus == 0:
-			item_map[d.item_code].setdefault('so_day_' + str(i+1), 0)
-			item_map[d.item_code]['so_day_' + str(i+1)] += d.qty
-			item_map[d.item_code]['draft_so_qty'] += d.qty
+	filters.from_date = filters.date
+	filters.to_date = frappe.utils.add_days(filters.from_date, 4)
+	po_data, so_data, sinv_data = get_order_qty_data(filters.from_date, filters.to_date, filters, conditions,
+		get_daily=True)
 
-	for d in sinv_data:
-		item_map.setdefault(d.item_code, template.copy())
-		i = frappe.utils.date_diff(d.date, filters.from_date)
-		item_map[d.item_code]['item_code'] = d.item_code
-		item_map[d.item_code]['item_name'] = d.item_name
-		item_map[d.item_code]['total_so_qty'] += d.qty
-		item_map[d.item_code].setdefault('so_day_' + str(i+1), 0)
-		item_map[d.item_code]['so_day_' + str(i+1)] += d.qty
-		item_map[d.item_code]['draft_so_qty'] += d.qty
+	add_qty_to_item_map(so_data, 'so', set_daily=True)
+	add_qty_to_item_map(sinv_data, 'so', set_daily=True)
+	add_qty_to_item_map(po_data, 'po', set_daily=True)
 
-	for d in po_data:
-		item_map.setdefault(d.item_code, template.copy())
-		i = frappe.utils.date_diff(d.date, filters.from_date)
-		item_map[d.item_code]['item_code'] = d.item_code
-		item_map[d.item_code]['item_name'] = d.item_name
-		item_map[d.item_code]['po_day_' + str(i+1)] = d.qty
-		item_map[d.item_code]['total_po_qty'] += d.qty
-
+	bin_data = get_bin_data(filters, conditions)
 	for d in bin_data:
 		item_map.setdefault(d.item_code, template.copy())
 		item_map[d.item_code]['actual_qty'] = d.actual_qty
 		item_map[d.item_code]['item_code'] = d.item_code
 		item_map[d.item_code]['item_name'] = d.item_name
 
+	if filters.selected_to_date:
+		po_data, so_data, sinv_data = get_order_qty_data(filters.from_date, filters.selected_to_date, filters, conditions,
+			get_daily=False)
+
+		add_qty_to_item_map(so_data, 'selected_so', set_daily=False)
+		add_qty_to_item_map(sinv_data, 'selected_so', set_daily=False)
+		add_qty_to_item_map(po_data, 'selected_po', set_daily=False)
+
+		for d in item_map.values():
+			d.total_available_qty = d.actual_qty + d.total_selected_po_qty
+			d.short_excess = d.total_available_qty + d.total_selected_so_qty
+
 	data = sorted(item_map.values(), key=lambda d: (d.total_so_qty, d.actual_qty), reverse=True)
 	return data
 
+
+def get_order_qty_data(from_date, to_date, filters, item_conditions, get_daily):
+	filters = filters.copy()
+	filters.from_date = from_date
+	filters.to_date = to_date
+
+	sales_group_by = ", s.delivery_date" if get_daily else ""
+	purchase_group_by = ", p.schedule_date" if get_daily else ""
+
+	po_data = frappe.db.sql("""
+		select
+			i.item_code, i.item_name, p.schedule_date as date,
+			sum((i.qty - ifnull(i.received_qty, 0)) * i.conversion_factor) as qty
+		from `tabPurchase Order Item` i
+		inner join `tabPurchase Order` p on p.name = i.parent
+		where p.docstatus < 2 and p.status != 'Closed' and ifnull(i.received_qty, 0) < ifnull(i.qty, 0)
+			and p.schedule_date between %(from_date)s and %(to_date)s {0}
+		group by i.item_code {1}
+	""".format(item_conditions, purchase_group_by), filters, as_dict=1)
+
+	so_data = frappe.db.sql("""
+		select
+			i.item_code, i.item_name, s.delivery_date as date,
+			sum((i.qty - ifnull(i.delivered_qty, 0)) * i.conversion_factor) as qty
+		from `tabSales Order Item` i
+		inner join `tabSales Order` s on s.name = i.parent
+		where s.docstatus < 2 and ifnull(i.delivered_qty, 0) < ifnull(i.qty, 0) and s.status != 'Closed'
+			and s.delivery_date between %(from_date)s and %(to_date)s {0}
+		group by i.item_code {1}
+	""".format(item_conditions, sales_group_by), filters, as_dict=1)
+
+	sinv_data = frappe.db.sql("""
+		select
+			i.item_code, i.item_name, s.delivery_date as date,
+			sum(i.stock_qty) as qty
+		from `tabSales Invoice Item` i
+		inner join `tabSales Invoice` s on s.name = i.parent
+		where s.docstatus = 0 and ifnull(i.sales_order, '') = ''
+			and s.delivery_date between %(from_date)s and %(to_date)s {0}
+		group by i.item_code {1}
+	""".format(item_conditions, sales_group_by), filters, as_dict=1)
+
+	return po_data, so_data, sinv_data
+
+
+def get_bin_data(filters, item_conditions):
+	return frappe.db.sql("""
+		select bin.item_code, sum(bin.actual_qty) as actual_qty, i.item_name
+		from tabBin bin, tabItem i
+		where i.name = bin.item_code and i.is_sales_item=1 {0}
+		group by bin.item_code
+		having actual_qty != 0
+	""".format(item_conditions), filters, as_dict=1)
 
 def get_item_conditions(filters):
 	conditions = []
@@ -127,9 +143,21 @@ def get_columns(filters):
 		{"fieldname": "item_code", "label": _("Item Code"), "fieldtype": "Link", "options": "Item", "width": 80},
 		{"fieldname": "item_name", "label": _("Item Name"), "fieldtype": "Data", "width": 150},
 		{"fieldname": "actual_qty", "label": _("In Stock"), "fieldtype": "Float", "width": 70},
-		{"fieldname": "total_so_qty", "label": _("Total SO"), "fieldtype": "Float", "width": 70},
-		{"fieldname": "draft_so_qty", "label": _("Draft SO"), "fieldtype": "Float", "width": 70,
-			"is_so_qty": 1, "from_date": filters.date, "to_date": add_days(filters.date, 4)}
+	]
+
+	if filters.selected_to_date:
+		columns += [
+			{"fieldname": "total_selected_po_qty", "label": _("Total PO Selected Dates"), "fieldtype": "Float", "width": 120,
+				"is_po_qty": 1, "from_date": filters.date, "to_date": filters.selected_to_date},
+			{"fieldname": "total_available_qty", "label": _("Total Available Qty"), "fieldtype": "Float", "width": 120},
+			{"fieldname": "total_selected_so_qty", "label": _("Total SO Selected Dates"), "fieldtype": "Float", "width": 120,
+				"is_so_qty": 1, "from_date": filters.date, "to_date": filters.selected_to_date},
+			{"fieldname": "short_excess", "label": _("Short(-)/Excess"), "fieldtype": "Float", "width": 120},
+		]
+
+	columns += [
+		{"fieldname": "total_so_qty", "label": _("Total SO"), "fieldtype": "Float", "width": 70,
+			"is_so_qty": 1, "from_date": filters.date, "to_date": add_days(filters.date, 4)},
 	]
 	for i in range(5):
 		date = add_days(filters.date, i)
