45dea05009eb574f4a60c54e7338c8005199ab50
Saif Ur Rehman <saifi0102@gmail.com>
2020-04-23 17:10:56 +0500

    fix(Product List): refactor and context

--- erpnext/www/product_list.py
+++ erpnext/www/product_list.py
@@ -1,8 +1,9 @@
 from __future__ import unicode_literals
+
 import frappe
 from frappe import throw, _
 import frappe.defaults
-from frappe.utils import cint, flt, get_fullname, cstr, nowdate
+from frappe.utils import nowdate
 from six import iteritems
 from collections import OrderedDict
 from erpnext.shopping_cart.cart import _get_cart_quotation, get_party
@@ -10,47 +11,94 @@ from erpnext.utilities.product import get_price
 
 
 def get_context(context):
+	context.no_cache = True
+	context.parents = [{'title': _('Products'), 'route': 'product-list'}]
+
+	item_group = frappe.form_dict.item_group
+
+	if not item_group:
+		frappe.local.flags.redirect_location = "/products"
+		raise frappe.Redirect
+
+	if not frappe.db.get_value("Item Group", item_group, 'show_in_website'):
+		context.title = _("Invalid Item Group")
+		raise frappe.DoesNotExistError
+
+	context.title = item_group
+
 	stock_settings = frappe.get_single("Stock Settings")
 	selling_settings = frappe.get_single("Selling Settings")
 	cart_settings = frappe.get_single("Shopping Cart Settings")
 
-	item_conditions = []
+	item_data = get_items(stock_settings, item_group=item_group)
+	item_group_map = group_by_item_group(item_data, stock_settings)
+	item_code_map = group_by_item_code(item_data)
 
-	item_groups_excluded = [d.item_group for d in stock_settings.price_list_excluded or []]
-	if item_groups_excluded:
-		item_conditions.append("item.item_group not in ('{}')".format("', '".join([frappe.db.escape(d) for d in item_groups_excluded])))
+	party = get_party() if frappe.session.user != "Guest" else frappe._dict()
 
-	item_groups_filter = []
-	item_group_arg = frappe.form_dict.item_group
-	if item_group_arg:
-		lft_rgt = frappe.db.get_value("Item Group", item_group_arg, ['lft', 'rgt'])
-		if lft_rgt:
-			lft, rgt = lft_rgt
-			item_groups_filter = frappe.db.sql_list("select name from `tabItem Group` where lft >= %s and rgt <= %s",
-				[lft, rgt])
+	price_list = party.default_price_list or cart_settings.price_list or selling_settings.selling_price_list
+	customer_group = party.customer_group or cart_settings.default_customer_group or selling_settings.customer_group
+	set_item_prices(item_data, price_list, customer_group, cart_settings.company)
 
-	if item_groups_filter:
-		item_conditions.append("item.item_group in ('{}')".format("', '".join([frappe.db.escape(d) for d in item_groups_filter])))
+	if party:
+		quotation = _get_cart_quotation(party)
+		set_quotation_item_details(item_code_map, quotation)
 
+	context.item_group_map = item_group_map
+
+
+def get_items(stock_settings, item_group=None, item_code=None):
+	conditions = []
 	filters = frappe._dict({
 		'today': nowdate()
 	})
 
-	item_data = frappe.db.sql("""
+	if item_code:
+		conditions.append("item.name = %(item_code)s")
+		filters['item_code'] = item_code
+	else:
+		# standard list filters
+		conditions.append("item.print_in_price_list = 1")
+
+		# excluded item groups
+		excluded_item_groups = [d.item_group for d in stock_settings.price_list_excluded or []]
+		if excluded_item_groups:
+			conditions.append("item.item_group not in %(excluded_item_groups)s")
+			filters['excluded_item_groups'] = excluded_item_groups
+
+		# filtered item groups
+		filtered_item_groups = []
+		if item_group:
+			lft_rgt = frappe.db.get_value("Item Group", item_group, ['lft', 'rgt'])
+			if lft_rgt:
+				lft, rgt = lft_rgt
+				filtered_item_groups = frappe.db.sql_list("select name from `tabItem Group` where lft >= %s and rgt <= %s",
+					[lft, rgt])
+
+		if filtered_item_groups:
+			conditions.append("item.item_group in %(filtered_item_groups)s")
+			filters['filtered_item_groups'] = filtered_item_groups
+
+	return frappe.db.sql("""
 		select item.name as item_code, item.item_name, item.item_group, item.route,
 			item.stock_uom, item.sales_uom, item.alt_uom, item.alt_uom_size,
 			item.thumbnail, item.website_image, item.image,
 			item.country_of_origin
 		from tabItem item
-		left join tabCountry c on c.name = item.country_of_origin
-		where item.disabled != 1 and item.is_sales_item = 1 and item.show_in_website = 1 and item.print_in_price_list = 1
+		where item.disabled = 0 and item.is_sales_item = 1 and (item.show_in_website = 1 or item.show_variant_in_website = 1)
 		and (ifnull(item.end_of_life, '0000-00-00') = '0000-00-00' or item.end_of_life > %(today)s) and {0}
-	""".format(" and ".join(item_conditions)), filters, as_dict=1)
+	""".format(" and ".join(conditions)), filters, as_dict=1)
 
-	item_map = {}
+
+def group_by_item_code(item_data):
+	item_code_map = {}
 	for d in item_data:
-		item_map[d.item_code] = d
+		item_code_map[d.item_code] = d
 
+	return item_code_map
+
+
+def group_by_item_group(item_data, stock_settings):
 	item_group_unsorted = OrderedDict()
 	for d in item_data:
 		item_group_unsorted.setdefault(d.item_group, []).append(d)
@@ -66,23 +114,15 @@ def get_context(context):
 	for item_group, items in iteritems(item_group_unsorted):
 		item_group_sorted[item_group] = sorted(items, key=lambda d: d.item_name)
 
-	party = get_party() if frappe.session.user != "Guest" else frappe._dict()
+	return item_group_sorted
 
-	price_list = party.default_price_list or cart_settings.price_list or selling_settings.selling_price_list
-	customer_group = party.customer_group or cart_settings.default_customer_group or selling_settings.customer_group
-	set_item_prices(item_data, price_list, customer_group, cart_settings.company)
-
-	if party:
-		quotation = _get_cart_quotation(party)
-		set_quotation_item_details(item_map, quotation)
-
-	context.item_group_map = item_group_sorted
 
 def set_quotation_item_details(item_map, quotation):
 	for d in quotation.items:
 		item = item_map.get(d.item_code)
 		if item:
 			item['qty'] = d.qty
+
 
 def set_item_prices(item_data, price_list, customer_group, company):
 	for d in item_data:
