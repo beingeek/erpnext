ec3ee591aa614d9e4203c1f2939b82b810508835
Saif Ur Rehman <saifi0102@gmail.com>
2020-05-03 06:51:16 +0500

    fix(Quotation): Indicators for CPO

--- erpnext/selling/doctype/quotation/quotation_list.js
+++ erpnext/selling/doctype/quotation/quotation_list.js
@@ -1,17 +1,25 @@
 frappe.listview_settings['Quotation'] = {
 	add_fields: ["customer_name", "base_grand_total", "status",
-		"company", "currency", 'valid_till'],
+		"company", "currency", 'valid_till', 'confirmed_by_customer'],
 	get_indicator: function(doc) {
 		if(doc.status==="Submitted") {
 			if (doc.valid_till && doc.valid_till < frappe.datetime.nowdate()) {
 				return [__("Expired"), "darkgrey", "valid_till,<," + frappe.datetime.nowdate()];
 			} else {
-				return [__("Submitted"), "blue", "status,=,Submitted"];
+				return [__("To Create SO"), "red", "status,=,Submitted"];
 			}
 		} else if(doc.status==="Ordered") {
 			return [__("Ordered"), "green", "status,=,Ordered"];
 		} else if(doc.status==="Lost") {
 			return [__("Lost"), "darkgrey", "status,=,Lost"];
+		} else if(doc.docstatus == 0) {
+			if (doc.confirmed_by_customer) {
+				return [__("To Receive"), "orange", "confirmed_by_customer,=,1|docstatus,=,0"];
+			} else {
+				return [__("Draft"), "darkgrey", "confirmed_by_customer,=,0|docstatus,=,0"];
+			}
 		}
-	}
+	},
+	has_indicator_for_draft: 1,
+	filters: [["confirmed_by_customer", "=", "1"]]
 };
