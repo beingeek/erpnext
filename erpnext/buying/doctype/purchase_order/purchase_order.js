cfb6924bae27dbf77f04d0ce7553e59e2b097f8e
Saif Ur Rehman <saifi0102@gmail.com>
2020-06-17 23:28:59 +0500

    fix: Rename Make to Create because v12 merge fix

--- erpnext/buying/doctype/purchase_order/purchase_order.js
+++ erpnext/buying/doctype/purchase_order/purchase_order.js
@@ -149,7 +149,7 @@ erpnext.buying.PurchaseOrderController = erpnext.buying.BuyingController.extend(
 
 		if(doc.docstatus == 1 && doc.status != "Closed") {
 			if(flt(doc.per_received, 2) < 100 && allow_receipt) {
-				cur_frm.add_custom_button(__('Receipt'), this.make_purchase_receipt, __("Make"));
+				cur_frm.add_custom_button(__('Receipt'), this.make_purchase_receipt, __("Create"));
 
 				if(doc.is_subcontracted==="Yes") {
 					cur_frm.add_custom_button(__('Material to Supplier'),
@@ -159,10 +159,10 @@ erpnext.buying.PurchaseOrderController = erpnext.buying.BuyingController.extend(
 
 			if(flt(doc.per_completed, 2) < 100)
 				cur_frm.add_custom_button(__('Invoice'),
-					this.make_purchase_invoice, __("Make"));
+					this.make_purchase_invoice, __("Create"));
 
 			if(flt(doc.per_billed)==0 && doc.status != "Delivered" && flt(doc.per_completed, 2) < 100) {
-				cur_frm.add_custom_button(__('Payment'), cur_frm.cscript.make_payment_entry, __("Make"));
+				cur_frm.add_custom_button(__('Payment'), cur_frm.cscript.make_payment_entry, __("Create"));
 			}
 
 			if(!doc.auto_repeat) {
@@ -173,14 +173,14 @@ erpnext.buying.PurchaseOrderController = erpnext.buying.BuyingController.extend(
 
 			if(flt(doc.per_billed)==0 && flt(doc.per_completed, 2) < 100) {
 				this.frm.add_custom_button(__('Payment Request'),
-					function() { me.make_payment_request() }, __("Make"));
+					function() { me.make_payment_request() }, __("Create"));
 			}
 
-			cur_frm.page.set_inner_btn_group_as_primary(__("Make"));
+			cur_frm.page.set_inner_btn_group_as_primary(__("Create"));
 		}
 
 		if(doc.docstatus < 2 && !doc.__islocal) {
-			cur_frm.add_custom_button(__('Landed Cost Voucher'), this.make_landed_cost_voucher, __("Make"));
+			cur_frm.add_custom_button(__('Landed Cost Voucher'), this.make_landed_cost_voucher, __("Create"));
 		}
 	},
 
